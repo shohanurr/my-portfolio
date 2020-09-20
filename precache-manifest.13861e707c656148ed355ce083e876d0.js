d5edf9a62e922127f219c015a5f8ae704fd30e02
shohanurr <sohanbaf@gmail.com>
2020-09-20 18:23:11 +0600

    Updates

---
+++ precache-manifest.13861e707c656148ed355ce083e876d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5fc641a68e87a581880dbd84575799",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "beb8d976e2517d7aa754",
+    "url": "/my-portfolio/static/css/main.100f7120.chunk.css"
+  },
+  {
+    "revision": "4f41b3e34d0b0f23c471",
+    "url": "/my-portfolio/static/js/2.88f37073.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.88f37073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb8d976e2517d7aa754",
+    "url": "/my-portfolio/static/js/main.7055c359.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  }
+]);
